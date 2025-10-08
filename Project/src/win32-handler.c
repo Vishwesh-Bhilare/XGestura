5b922a4d1235feb4f22ac4ec961c505c5c205219
Dhananjay-Mane-Ithech-Rahtat <deoanimish07@gmail.com>
2025-10-09 01:04:22 +0530

    Keyboard inputs working:

--- Project/src/win32-handler.c
+++ Project/src/win32-handler.c
@@ -6,8 +6,28 @@
 // call-back function declarations
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+// window size macros
+#define WINDOW_WIDTH  440
+#define WINDOW_HEIGHT 160
+
+// global handle to window
+HWND ghwnd;
+
+// created buttons related
+HWND hRecordActionsButtonWnd;
+HWND hStopRecordingActionsButtonWnd;
+HWND hPerformRecordedActionsButtonWnd;
+
+// global linked list state variables
+BOOL gIsRecording = FALSE;
+ActionNode *gpHead = NULL;
+ActionNode *gpTail = NULL;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpszCmdLine, int nCmdShow)
 {
+    // function declarations
+    int initialize(void);
+    
     // variable declarations
     WNDCLASSEX wndclass;
     TCHAR szClassName[] = TEXT("MyWindowClass");
@@ -38,38 +58,216 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpszCmdL
     // create window of custom window class in memory
     hwnd = CreateWindow(szClassName, 
                         szWindowName,
-                        WS_OVERLAPPEDWINDOW,
+                        WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX,
                         CW_USEDEFAULT,
                         CW_USEDEFAULT,
-                        CW_USEDEFAULT,
-                        CW_USEDEFAULT,
+                        WINDOW_WIDTH,
+                        WINDOW_HEIGHT,
                         NULL,
                         NULL,
                         hInstance,
                         NULL);
+    
+    // set ghwnd to hwnd
+    ghwnd = hwnd;
 
     // show window from memory
     ShowWindow(hwnd, nCmdShow);
 
+    // update the window
+    UpdateWindow(hwnd);
+
+    // initialize
+    if (initialize())
+    {
+        MessageBox(NULL, TEXT("Failed to initialize the program"), TEXT("EXIT_ERROR"), MB_ICONERROR | MB_OK);
+        DestroyWindow(hwnd);
+    }
+
     // message pump
-    while(GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return((int)msg.wParam);
+    // cleanup linked list memory
+    FreeRecordedActions();
+
+    return ((int)msg.wParam);
 }
 
-// callback function bodu
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+int initialize(void)
 {
-    switch(iMsg)
+    // code
+    // precautionary sleep 
+    Sleep(50);
+
+    // create button and set properties
+    hRecordActionsButtonWnd = CreateWindow( 
+        TEXT("BUTTON"),  // Predefined class
+        TEXT("Record Actions"),      // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+        10,         // x position 
+        10,         // y position 
+        120,        // Button width
+        100,        // Button height
+        ghwnd,     // Parent window
+        (HMENU)IDM_RECORD_ACTIONS,       // menu aahe.
+        GetModuleHandle(NULL), 
+        NULL);      // Pointer not needed.
+
+    hStopRecordingActionsButtonWnd = CreateWindow(
+        TEXT("BUTTON"),  // Predefined class
+        TEXT("Stop Recording"),      // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+        10 + 140,         // x position 
+        10,         // y position 
+        120,        // Button width
+        100,        // Button height
+        ghwnd,     // Parent window
+        (HMENU)IDM_STOP_RECORD_ACTIONS,       // menu aahe.
+        GetModuleHandle(NULL), 
+        NULL);      // Pointer not needed. 
+
+    hPerformRecordedActionsButtonWnd = CreateWindow(
+        TEXT("BUTTON"),  // Predefined class
+        TEXT("Perform Actions"),      // Button text 
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+        10 + 140 + 140,         // x position 
+        10,         // y position 
+        120,        // Button width
+        100,        // Button height
+        ghwnd,     // Parent window
+        (HMENU)IDM_PERFORM_ACTIONS,       // menu aahe.
+        GetModuleHandle(NULL), 
+        NULL);      // Pointer not needed. 
+
+    return (0);
+}
+
+// function to add a recorded input to linked list
+void AddAction(INPUT in)
+{
+    ActionNode *node = (ActionNode*)malloc(sizeof(ActionNode));
+    if (!node)
     {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
+        return;
     }
 
-    return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+    node->input = in;
+    node->next = NULL;
+
+    if (!gpHead)
+    {
+        gpHead = gpTail = node;
+    }
+    else
+    {
+        gpTail->next = node;
+        gpTail = node;
+    }
+}
+
+// function to play back all recorded inputs
+void PlayRecordedActions(void)
+{
+    if (!gpHead)
+    {
+        MessageBox(ghwnd, TEXT("No actions recorded!"), TEXT("INFO"), MB_OK | MB_ICONINFORMATION);
+        return;
+    }
+
+    ActionNode *temp = gpHead;
+    while (temp)
+    {
+        SendInput(1, &temp->input, sizeof(INPUT));
+        Sleep(50); // delay between inputs
+        temp = temp->next;
+    }
+
+    MessageBox(ghwnd, TEXT("Playback completed!"), TEXT("INFO"), MB_OK | MB_ICONINFORMATION);
+}
+
+// function to free linked list
+void FreeRecordedActions(void)
+{
+    ActionNode *temp;
+    while (gpHead)
+    {
+        temp = gpHead;
+        gpHead = gpHead->next;
+        free(temp);
+    }
+    gpTail = NULL;
+}
+
+// callback function body
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (iMsg)
+    {
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            break;
+        }
+
+        case WM_KEYDOWN:
+        case WM_KEYUP:
+        {
+            if (gIsRecording)
+            {
+                INPUT in = {0};
+                in.type = INPUT_KEYBOARD;
+                in.ki.wVk = (WORD)wParam;
+                in.ki.dwFlags = (iMsg == WM_KEYUP) ? KEYEVENTF_KEYUP : 0;
+                AddAction(in);
+            }
+            break;
+        }
+
+        case WM_LBUTTONDOWN:
+        case WM_LBUTTONUP:
+        {
+            if (gIsRecording)
+            {
+                INPUT in = {0};
+                in.type = INPUT_MOUSE;
+                in.mi.dwFlags = (iMsg == WM_LBUTTONDOWN) ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_LEFTUP;
+                AddAction(in);
+            }
+            break;
+        }
+
+        case WM_COMMAND:
+        {
+            switch (LOWORD(wParam))
+            {
+                case IDM_RECORD_ACTIONS:
+                {
+                    gIsRecording = TRUE;
+                    FreeRecordedActions(); // clear old data
+                    MessageBox(hwnd, TEXT("Recording started!"), TEXT("INFO"), MB_OK);
+                    break;
+                }
+
+                case IDM_STOP_RECORD_ACTIONS:
+                {
+                    gIsRecording = FALSE;
+                    MessageBox(hwnd, TEXT("Recording stopped!"), TEXT("INFO"), MB_OK);
+                    break;
+                }
+
+                case IDM_PERFORM_ACTIONS:
+                {
+                    PlayRecordedActions();
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    return (DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
--- Project/src/win32-handler.h
+++ Project/src/win32-handler.h
@@ -1,2 +1,27 @@
+#pragma once
+
 // window icon related
 #define APP_ICON 101
+
+// buttons menu macros
+#define IDM_RECORD_ACTIONS      201
+#define IDM_STOP_RECORD_ACTIONS 202
+#define IDM_PERFORM_ACTIONS     203
+
+
+// linked list and recording related declarations
+typedef struct ActionNode
+{
+    INPUT input;
+    struct ActionNode *next;
+} ActionNode;
+
+// global variable declarations
+extern BOOL gIsRecording;
+extern ActionNode *gpHead;
+extern ActionNode *gpTail;
+
+// function declarations
+void AddAction(INPUT in);
+void PlayRecordedActions(void);
+void FreeRecordedActions(void);
